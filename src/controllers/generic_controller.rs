c360b15a5ad516f5e611a456c2e7783add5ec982
Pjiwm <pim.munne@gmail.com>
2023-05-01 20:28:45 +0200

    generic controller uses state fully

--- src/controllers/generic_controller.rs
+++ src/controllers/generic_controller.rs
@@ -1,10 +1,9 @@
 use crate::models::PayloadConstructor;
-use crate::mongo::collection;
 use crate::utils::jwt::{self, Claims};
 use axum::extract::{Path, State};
 use axum::http::StatusCode;
 use axum::response::{IntoResponse, Json};
-use futures::TryStreamExt;
+use futures::StreamExt;
 use mongodb::bson::ser;
 use mongodb::bson::{doc, oid::ObjectId};
 use mongodb::Database;
@@ -54,77 +53,84 @@ pub async fn create<T: Bounds>(
     }
 }
 /// Returns one object of the generic type given in JSON format.
-pub async fn get_by_id<T: Bounds>(Path(id): Path<String>) -> impl IntoResponse {
-    let mongo_id = ObjectId::parse_str(id);
-    if mongo_id.is_err() {
-        return (StatusCode::NOT_FOUND, Json(json!({"error": "Not found"})));
+pub async fn get_by_id<T: Bounds>(
+    State(db): State<Arc<Database>>,
+    Path(id): Path<String>,
+) -> impl IntoResponse {
+    let collection = db.collection::<T>(&T::name());
+    let filter = match ObjectId::parse_str(id) {
+        Ok(id) => doc! {"_id": id},
+        Err(_) => return (StatusCode::NOT_FOUND, Json(json!({"error": "Not found"}))),
+    };
+    match collection.find_one(filter, None).await {
+        Ok(Some(r)) => match serde_json::to_value(&r) {
+            Ok(v) => (StatusCode::OK, Json(v)),
+            Err(_) => (StatusCode::NOT_FOUND, Json(json!({"status": "not_found"}))),
+        },
+        _ => (StatusCode::NOT_FOUND, Json(json!({"status": "not_found"}))),
     }
-    let filter = doc! {"_id": mongo_id.unwrap()};
-    let object = collection::<T>()
-        .await
-        .find_one(filter, None)
-        .await
-        .unwrap();
-    let json = Json(serde_json::to_value(&object).unwrap());
-    match &object {
-        None => return (StatusCode::NOT_FOUND, Json(json!({"status": "not_found"}))),
-        _ => (),
-    }
-    return (StatusCode::OK, json);
 }
 /// Returns all object of the generic type given in JSON format.
-pub async fn get_all<T: Bounds>() -> impl IntoResponse {
-    let mut objects = collection::<T>().await.find(None, None).await.unwrap();
-    let mut json: Vec<Value> = Vec::new();
-
-    while let Some(object) = objects.try_next().await.expect(r#"something went wrong"#) {
-        json.push(serde_json::to_value(&object).unwrap());
+pub async fn get_all<T: Bounds>(State(db): State<Arc<Database>>) -> impl IntoResponse {
+    match db.collection::<T>(&T::name()).find(None, None).await {
+        Ok(v) => {
+            let json: Vec<Value> = v
+                .filter_map(|x| async move { x.ok() })
+                .collect::<Vec<T>>()
+                .await
+                .iter()
+                .filter_map(|x| serde_json::to_value(x).ok())
+                .collect();
+            (StatusCode::OK, Json(json!({ "objects": json })))
+        }
+        Err(_) => (StatusCode::OK, Json(json!({ "objects": [] }))),
     }
-    return (StatusCode::OK, Json(json!({ "objects": json })));
 }
 /// Updates one object of the generic type.
 pub async fn update<T: Bounds>(
     Path(id): Path<String>,
+    State(db): State<Arc<Database>>,
     Json(payload): Json<Value>,
 ) -> impl IntoResponse {
-    let mongo_id = ObjectId::parse_str(id);
-    if mongo_id.is_err() {
-        return (StatusCode::NOT_FOUND, Json(json!({"error": "Not found"})));
-    }
-    let filter = doc! {"_id": mongo_id.unwrap()};
-    // remove if we make generic controller
-    let doc = ser::to_bson(&payload);
-    match doc {
+    let collection = db.collection::<T>(&T::name());
+    let filter = match ObjectId::parse_str(id) {
+        Ok(id) => doc! {"_id": id},
+        Err(_) => return (StatusCode::NOT_FOUND, Json(json!({"error": "Not found"}))),
+    };
+    match ser::to_bson(&payload) {
         Ok(d) => {
-            let doc = d.as_document().unwrap();
-            let result = collection::<T>()
-                .await
-                .update_one(filter, doc! {"$set":doc}, None)
-                .await
-                .ok();
-            return (StatusCode::OK, Json(json!({ "status": result })));
+            if let Some(doc) = d.as_document() {
+                let result = collection
+                    .update_one(filter, doc! {"$set":doc}, None)
+                    .await
+                    .ok();
+                (StatusCode::OK, Json(json!({ "status": result })))
+            } else {
+                (
+                    StatusCode::BAD_REQUEST,
+                    Json(json!({"error": "Could not convert to document"})),
+                )
+            }
         }
-        Err(e) => {
-            return (
-                StatusCode::BAD_REQUEST,
-                Json(json!({"error": e.to_string()})),
-            )
-        }
+        Err(e) => (
+            StatusCode::BAD_REQUEST,
+            Json(json!({"error": e.to_string()})),
+        ),
     }
 }
 /// Removes one object of the generic type.
-pub async fn remove<T: Bounds>(Path(id): Path<String>) -> impl IntoResponse {
-    let mongo_id = ObjectId::parse_str(id);
-    if mongo_id.is_err() {
-        return (StatusCode::NOT_FOUND, Json(json!({"error": "Not found"})));
-    }
-    let filter = doc! {"_id": mongo_id.unwrap()};
-    let result = collection::<T>().await.delete_one(filter, None).await.ok();
-    // this is similar as what was commented on update, we change the result to json so we can grab the value deletedCount
-    // if it didn't delete anything we give a status of not found.
-    let result_as_json = Json(serde_json::to_value(&result).unwrap());
-    if result_as_json["deletedCount"].as_i64().unwrap() == 0 {
-        return (StatusCode::NOT_FOUND, Json(json!({"status": "not_found"})));
-    }
+pub async fn remove<T: Bounds>(
+    Path(id): Path<String>,
+    State(db): State<Arc<Database>>,
+) -> impl IntoResponse {
+    let filter = match ObjectId::parse_str(id) {
+        Ok(id) => doc! {"_id": id},
+        Err(_) => return (StatusCode::NOT_FOUND, Json(json!({"error": "Not found"}))),
+    };
+    let result = db
+        .collection::<T>(&T::name())
+        .delete_one(filter, None)
+        .await
+        .ok();
     return (StatusCode::OK, Json(json!({ "status": result })));
 }
